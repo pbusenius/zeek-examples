ddfc7fdb8c91b2aeec7c3c137614cf6293152300
pbusenius <patrick.busenius@gmail.com>
2024-11-28 12:15:49 +0100

    first commit

---
+++ commands/zeek.py
@@ -0,0 +1,45 @@
+import os
+import shutil
+import subprocess
+from zat.log_to_dataframe import LogToDataFrame
+
+
+OUTPUT_DIRECTOR = "log"
+
+
+def read_files_log():
+    log_to_df = LogToDataFrame()
+    try:
+        df = log_to_df.create_dataframe(f"{OUTPUT_DIRECTOR}/ssh.log")
+    except OSError:
+        return 
+    
+    df = df.dropna(subset=["auth_success"])
+
+    df = df[df["auth_success"]=="T"]
+
+    df = df.reset_index()
+
+    print(df)
+
+
+def execute_zeek_command():
+    subprocess.run(["zeek", "-C", "-r", "data/smb1.pcap", "Log::default_logdir=log"])
+
+
+def clean_log_direcotry():
+    if os.path.exists(OUTPUT_DIRECTOR):
+        shutil.rmtree(OUTPUT_DIRECTOR)
+
+    os.mkdir(OUTPUT_DIRECTOR)
+
+
+def main():
+    clean_log_direcotry()
+    execute_zeek_command()
+
+    read_files_log()
+
+
+if __name__ == "__main__":
+    main()
---
+++ commands/zeek_file_export.py
@@ -0,0 +1,61 @@
+import os
+import shutil
+import subprocess
+from zat.log_to_dataframe import LogToDataFrame
+
+
+OUTPUT_DIRECTOR = "logs"
+
+
+def read_files_log():
+    log_to_df = LogToDataFrame()
+    df = log_to_df.create_dataframe(f"{OUTPUT_DIRECTOR}/files.log")
+
+    df = df[
+        [
+            "id.orig_h",
+            "id.resp_h",
+            "filename",
+            "source",
+            "total_bytes",
+            "missing_bytes",
+            "extracted_size",
+        ]
+    ]
+
+    df = df.reset_index()
+
+    print(df)
+
+
+def execute_zeek_command():
+    subprocess.run(
+        [
+            "zeek",
+            "-C",
+            "-r",
+            "data/test.pcap",
+            "frameworks/files/extract-all-files",
+            f"Log::default_logdir={OUTPUT_DIRECTOR}",
+            f"FileExtract::prefix={OUTPUT_DIRECTOR}/test",
+        ]
+    )
+
+
+def clean_log_direcotry():
+    if os.path.exists(OUTPUT_DIRECTOR):
+        shutil.rmtree(OUTPUT_DIRECTOR)
+
+    os.mkdir(OUTPUT_DIRECTOR)
+
+
+def main():
+    clean_log_direcotry()
+
+    execute_zeek_command()
+
+    read_files_log()
+
+
+if __name__ == "__main__":
+    main()

7d16f1c3a80692e2ff4423f3dc2869a251f6a701
pbusenius <patrick.busenius@gmail.com>
2024-11-28 15:51:02 +0100

    added zeek ssh

--- commands/zeek_ssh.py
+++ commands/zeek_ssh.py
@@ -4,21 +4,28 @@ import subprocess
 from zat.log_to_dataframe import LogToDataFrame
 
 
+PCAP_FILE = "data/ssh.pcap"
 OUTPUT_DIRECTOR = "logs"
 
 
 def read_files_log():
     log_to_df = LogToDataFrame()
     try:
-        df = log_to_df.create_dataframe(f"{OUTPUT_DIRECTOR}/conn.log")
+        df = log_to_df.create_dataframe(f"{OUTPUT_DIRECTOR}/ssh.log")
     except OSError:
         return
+
+    df = df.dropna(subset=["auth_success"])
+
+    df = df[df["auth_success"] == "T"]
+
+    df = df.reset_index()
 
     print(df)
 
 
 def execute_zeek_command():
-    subprocess.run(["zeek", "-C", "-r", "data/test.pcap", "Log::default_logdir=logs"])
+    subprocess.run(["zeek", "-C", "-r", PCAP_FILE, f"Log::default_logdir={OUTPUT_DIRECTOR}"])
 
 
 def clean_log_direcotry():
